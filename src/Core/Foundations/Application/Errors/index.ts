ed9c0f8fdd22bafcfbb8816ea71d4d1bdb7102d5
heshamalsaqqaf2 <heshamdev2@gmail.com>
2026-06-11 03:11:12 +0300

    feat(04-02): create Errors and Results barrel files
    - Errors/index.ts exports ApplicationError, isApplicationError, ApplicationErrorValidator
    - Results/index.ts exports ApplicationResult type alias
    - (Specific/ barrel added in Plan 04-03)

---
+++ src/Core/Foundations/Application/Errors/index.ts
@@ -0,0 +1,3 @@
+export { ApplicationError } from "./application-error";
+export { isApplicationError } from "./application-error.type-guard";
+export { ApplicationErrorValidator } from "./application-error.validator";
---
+++ src/Core/Foundations/Application/Results/index.ts
@@ -0,0 +1,1 @@
+export type { ApplicationResult } from "./application-result";
