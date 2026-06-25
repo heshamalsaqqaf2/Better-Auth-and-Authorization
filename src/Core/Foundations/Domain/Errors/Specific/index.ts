d7e599c0dc6dae503774381265dddfcbfa7be80e
heshamalsaqqaf2 <heshamdev2@gmail.com>
2026-06-25 03:28:43 +0300

    Remove Specific Dir In Foundations Domain And Update index.ts In Remove export Specific.

--- src/Core/Foundations/Domain/Errors/Specific/index.ts
+++
@@ -1,3 +0,0 @@
-export { ComplaintAlreadyExistsError } from "./complaint-already-exists.error";
-export { ComplaintNotFoundError } from "./complaint-not-found.error";
-export { UserNotFoundError } from "./user-not-found.error";
--- src/Core/Foundations/Domain/Errors/index.ts
+++ src/Core/Foundations/Domain/Errors/index.ts
@@ -1,4 +1,3 @@
 export { DomainError } from "./domain-error";
 export { isDomainError } from "./domain-error.type-guard";
 export { DomainErrorValidator } from "./domain-error.validator";
-export * from "./Specific";
