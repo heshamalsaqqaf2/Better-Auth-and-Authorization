dd3156b54c926917899536837f34e125806c046d
heshamalsaqqaf2 <heshamdev2@gmail.com>
2026-07-18 02:38:12 +0300

    feat(08-09): update components barrel with SignOutButton and UserNav exports

--- src/Modules/Authentication/Presentation/Components/index.ts
+++ src/Modules/Authentication/Presentation/Components/index.ts
@@ -1,2 +1,4 @@
 export { SignInForm } from "./sign-in-form";
+export { default as SignOutButton } from "./sign-out-button";
 export { SignUpForm } from "./sign-up-form";
+export { default as UserNav } from "./user-nav";
