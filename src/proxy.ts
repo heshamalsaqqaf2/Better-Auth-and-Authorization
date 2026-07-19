4d95aa948e2e31d8d998fc2ceb5c5d41685eb849
heshamalsaqqaf2 <heshamdev2@gmail.com>
2026-07-20 00:07:57 +0300

    Delete Code Commited.

--- src/proxy.ts
+++ src/proxy.ts
@@ -28,23 +28,3 @@ export async function proxy(request: NextRequest): Promise<NextResponse<unknown>
 export const config = {
   matcher: ["/dashboard"],
 };
-
-// import { headers } from "next/headers";
-// import { type NextRequest, NextResponse } from "next/server";
-// import { auth } from "./Lib/BetterAuth/Config/server";
-
-// export async function proxy(request: NextRequest) {
-//   const session = await auth.api.getSession({
-//     headers: await headers(),
-//   });
-
-//   if (!session) {
-//     return NextResponse.redirect(new URL("/sign-in", request.url));
-//   }
-
-//   return NextResponse.next();
-// }
-
-// export const config = {
-//   matcher: ["/dashboard"], // Specify the routes the middleware applies to
-// };
